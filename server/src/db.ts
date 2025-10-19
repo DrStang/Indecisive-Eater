3de09b1d5f66934afa9831a44644fd18d38b2742
DrStang <48525932+DrStang@users.noreply.github.com>
2025-10-19 10:42:19 -0400

    Update db.ts

--- server/src/db.ts
+++ server/src/db.ts
@@ -7,8 +7,9 @@ export const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: true, minVersion: 'TLSv1.2' },
+    //ssl: { rejectUnauthorized: true, minVersion: 'TLSv1.2' },
     connectionLimit: 10,
     namedPlaceholders: true
 
 });
+
